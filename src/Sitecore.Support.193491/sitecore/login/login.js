a948a093011acf9d0dbf5ad2dfe15b15ddb39158
DK\IRO <iro@sitecore.net>
2017-11-14 18:34:53 +0200

    added the check for null

--- src/Sitecore.Support.193491/sitecore/login/login.js
+++ src/Sitecore.Support.193491/sitecore/login/login.js
@@ -1,29 +1,34 @@
 ﻿// Note: Disabling and enabling to make bootstrap field validation work
 
 window.onload = function () {
-    document.getElementsByClassName("show-recovery")[0].onclick = function () {
-        document.getElementById("login").style.display = "none";
-        document.getElementById("passwordRecovery").style.display = "block";
-        var userNameForgotNode = document.getElementById("UserNameForgot");
-        if (userNameForgotNode != null) {
-            userNameForgotNode.disabled = false;
-            userNameForgotNode.focus();
-        }
-        document.getElementById("UserName").disabled = true;
-        document.getElementById("Password").disabled = true;
-    };
-
-    document.getElementsByClassName("hide-recovery")[0].onclick = function () {
-        document.getElementById("passwordRecovery").style.display = "none";
-        document.getElementById("login").style.display = "block";
-        var userNameForgotNode = document.getElementById("UserNameForgot");
-        if (userNameForgotNode != null) {
-            userNameForgotNode.disabled = true;
-        }
-        document.getElementById("UserName").disabled = false;
-        document.getElementById("Password").disabled = false;
-        document.getElementById("UserName").focus();
-    };
+    var showRecovery = document.getElementsByClassName("show-recovery");
+    if (showRecovery != null && showRecovery.length != 0) {
+        showRecovery[0].onclick = function () {
+            document.getElementById("login").style.display = "none";
+            document.getElementById("passwordRecovery").style.display = "block";
+            var userNameForgotNode = document.getElementById("UserNameForgot");
+            if (userNameForgotNode != null) {
+                userNameForgotNode.disabled = false;
+                userNameForgotNode.focus();
+            }
+            document.getElementById("UserName").disabled = true;
+            document.getElementById("Password").disabled = true;
+        };
+    }
+    var hideRecovery = document.getElementsByClassName("hide-recovery");
+    if (hideRecovery != null && hideRecovery.length != 0) {
+        hideRecovery[0].onclick = function () {
+            document.getElementById("passwordRecovery").style.display = "none";
+            document.getElementById("login").style.display = "block";
+            var userNameForgotNode = document.getElementById("UserNameForgot");
+            if (userNameForgotNode != null) {
+                userNameForgotNode.disabled = true;
+            }
+            document.getElementById("UserName").disabled = false;
+            document.getElementById("Password").disabled = false;
+            document.getElementById("UserName").focus();
+        };
+    }
 
     document.querySelector("#login input[type='submit']").onclick = function () {
         if (document.getElementById("UserName").value === "" || document.getElementById("Password").value === "") {
