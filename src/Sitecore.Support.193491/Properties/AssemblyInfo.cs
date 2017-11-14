15a559d013535a7719a131440d8502e7c3819362
DK\IRO <iro@sitecore.net>
2017-11-14 18:30:23 +0200

    Initial

---
+++ src/Sitecore.Support.193491/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.193491")]
+[assembly: AssemblyProduct("Sitecore.Support.193491")]
+[assembly: ComVisible(false)]
